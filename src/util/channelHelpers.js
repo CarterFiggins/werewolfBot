28589dba8779878c1bc1a53a077dd12f9e86f602
Carter Figgins <Carter.Figgins@atomicjolt.com>
2022-03-22 23:33:58 -0600

    fix printing error

--- src/util/channelHelpers.js
+++ src/util/channelHelpers.js
@@ -47,6 +47,7 @@ async function sendStartMessages(interaction, users) {
   });
 
   // Option to show what characters will be playing
+  const printPlayers = false;
   if (printPlayers) {
     organizedChannels.townSquare.send(
       `${townSquareStart}\nCharacters in game:\n${printCharacters}`
