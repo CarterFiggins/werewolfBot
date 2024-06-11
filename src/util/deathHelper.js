dc6e075c22e5f71e3fc208d08bafd056029ed0c6
Carter Figgins <Carter.Figgins@atomicjolt.com>
2024-06-11 08:15:35 -0600

    differnet death by werewolf text

--- src/util/deathHelper.js
+++ src/util/deathHelper.js
@@ -282,7 +282,7 @@ async function werewolfKillDeathMessage({ interaction, deadMember, deadUser }) {
      return `Last night, the werewolves attacked and injured the **${deathCharacter}**, ${deadMember}. Despite their injuries, the hunter has one last chance to fight back. ${deadMember} now has the opportunity to shoot any player with their gun before succumbing to their wounds.\nChoose wisely, ${deadMember}. The fate of the village may rest on your final shot.\n`;
   }
   
-  return `Last night, the werewolves struck again. ${deadMember}, who played the role of ${deathCharacter}, has been killed by the attack.\n`;
+  return `Last night, ${deadMember} was found dead, playing the role of ${deathCharacter}. They have been killed by a werewolf attack.\n`;
 }
 
 async function sendGunDeathMessage({ interaction, deadCharacter, deadTargetMember, targetDbUser, memberWhoShot, randomFire }) {
