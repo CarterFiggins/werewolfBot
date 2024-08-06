83aaf8fbed55216ea803ada5deeb881b323bbedc
Carter Figgins <Carter.Figgins@atomicjolt.com>
2024-08-06 17:26:30 -0600

    added select menu for settings and refacter the settings command

--- src/commands/settings.js
+++ src/commands/settings.js
@@ -4,41 +4,26 @@ const { commandNames } = require("../util/commandHelpers");
 const { permissionCheck } = require("../util/permissionCheck");
 const { isAdmin } = require("../util/rolesHelpers");
 const { updateSettings, findSettings } = require("../werewolf_db");
+const { settingsList } = require("../util/botMessages/settings");
 
-const SettingCommands = {
+const valueSettings = {
   DAY_TIME: "day_time",
   NIGHT_TIME: "night_time",
-  RANDOM_CARDS: "random_cards",
-  CAN_WHISPER: "can_whisper",
-  ALLOW_REACTIONS: "allow_reactions",
-  EXTRA_CHARACTERS: "extra_characters",
-  WOLF_KILLS_WITCH: "wolf_kills_witch",
-  HARD_MODE: "hard_mode",
-  ALLOW_CHAOS_DEMON: 'allow_chaos_demon',
-  ALLOW_VAMPIRES: "allow_vampires",
-  ALLOW_FIRST_BITE: "allow_first_bite",
-  KING_BITE_WOLF_SAFE: "king_bite_wolf_safe",
-  KING_VICTIM_ATTACK_SAFE: "king_victim_attack_safe",
   VIEW: "view",
-  BODYGUARD_JOINS_MASONS: "bodyguard_joins_masons",
-  SEER_JOINS_MASONS: "seer_joins_masons",
-  HUNTER_GUARD: "hunter_guard",
-  ALLOW_LYCAN_GUARD: "allow_lycan_guard",
-  ENABLE_POWER_UPS: "enable_power_ups",
-};
+}
 
-module.exports = {
-  data: new SlashCommandBuilder()
+function buildSlashCommand() {
+  const builder = new SlashCommandBuilder()
     .setName(commandNames.SETTINGS)
     .setDescription("settings for game")
     .addSubcommand((subcommand) =>
       subcommand
-        .setName(SettingCommands.VIEW)
+        .setName(valueSettings.VIEW)
         .setDescription("view current settings")
     )
     .addSubcommand((subcommand) =>
       subcommand
-        .setName(SettingCommands.DAY_TIME)
+        .setName(valueSettings.DAY_TIME)
         .setDescription(
           "ADMIN COMMAND: The time when day starts. The bot will announce who died durning the night"
         )
@@ -57,7 +42,7 @@ module.exports = {
     )
     .addSubcommand((subcommand) =>
       subcommand
-        .setName(SettingCommands.NIGHT_TIME)
+        .setName(valueSettings.NIGHT_TIME)
         .setDescription(
           "ADMIN COMMAND: The time when night starts. The bot announce who will be hanged"
         )
@@ -76,10 +61,22 @@ module.exports = {
     )
     .addSubcommand((subcommand) =>
       subcommand
-        .setName(SettingCommands.RANDOM_CARDS)
+        .setName('edit')
         .setDescription(
-          "ADMIN COMMAND: no set limit on any characters and chosen at random"
+          "ADMIN COMMAND: Turn on and off settings"
         )
+        .addStringOption((option) => {
+          option
+            .setName("setting")
+            .setDescription("The name of the setting")
+            .setRequired(true)
+            _.forEach(settingsList, (setting) => {
+              if (setting.id) {
+                option.addChoices({ name: setting.label, value: setting.id })
+              }
+            })
+          return option
+        })
         .addBooleanOption((option) =>
           option
             .setName("activate")
@@ -87,187 +84,12 @@ module.exports = {
             .setDescription("turn setting off or on")
         )
     )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName(SettingCommands.CAN_WHISPER)
-        .setDescription(
-          "ADMIN COMMAND: The ability to whisper to another player using the bot"
-        )
-        .addBooleanOption((option) =>
-          option
-            .setName("activate")
-            .setRequired(true)
-            .setDescription("turn setting off or on")
-        )
-    )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName(SettingCommands.ALLOW_REACTIONS)
-        .setDescription(
-          "ADMIN COMMAND: The ability to react to another player's massage in game channels"
-        )
-        .addBooleanOption((option) =>
-          option
-            .setName("activate")
-            .setRequired(true)
-            .setDescription("Set setting true or false")
-        )
-    )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName(SettingCommands.EXTRA_CHARACTERS)
-        .setDescription(
-          "ADMIN COMMAND: Characters added: mutated villager, witch, fool, doppelganger, and apprentice seer"
-        )
-        .addBooleanOption((option) =>
-          option
-            .setName("activate")
-            .setRequired(true)
-            .setDescription("Set setting true or false")
-        )
-    )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName(SettingCommands.WOLF_KILLS_WITCH)
-        .setDescription(
-          "ADMIN COMMAND: If true wolf kills the witch. If false the witch will join the werewolf chat"
-        )
-        .addBooleanOption((option) =>
-          option
-            .setName("activate")
-            .setRequired(true)
-            .setDescription("Set setting true or false")
-        )
-    )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName(SettingCommands.HARD_MODE)
-        .setDescription(
-          "ADMIN COMMAND: When player dies there character will not be reviled and will not see team count"
-        )
-        .addBooleanOption((option) =>
-          option
-            .setName("activate")
-            .setRequired(true)
-            .setDescription("Set setting true or false")
-        )
-  )
-  .addSubcommand((subcommand) =>
-    subcommand
-      .setName(SettingCommands.ALLOW_CHAOS_DEMON)
-      .setDescription("ADMIN COMMAND: Allow the Chaos Demon character to play")
-      .addBooleanOption((option) =>
-        option
-          .setName("activate")
-          .setRequired(true)
-          .setDescription("Set setting true or false")
-      )
-  )
-  .addSubcommand((subcommand) =>
-    subcommand
-      .setName(SettingCommands.ALLOW_VAMPIRES)
-      .setDescription("ADMIN COMMAND: Allow the vampire character to play")
-      .addBooleanOption((option) =>
-        option
-          .setName("activate")
-          .setRequired(true)
-          .setDescription("Set setting true or false")
-      )
-  )
-  .addSubcommand((subcommand) =>
-    subcommand
-      .setName(SettingCommands.ALLOW_FIRST_BITE)
-      .setDescription(
-        "ADMIN COMMAND: Vampires king's first bite will transform a player into a vampire"
-      )
-      .addBooleanOption((option) =>
-        option
-          .setName("activate")
-          .setRequired(true)
-          .setDescription("Set setting true or false")
-      )
-    )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName(SettingCommands.KING_BITE_WOLF_SAFE)
-        .setDescription(
-          "ADMIN COMMAND: The vampire king will not die when trying to bite a werewolf"
-        )
-        .addBooleanOption((option) =>
-          option
-            .setName("activate")
-            .setRequired(true)
-            .setDescription("Set setting true or false")
-        )
-    )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName(SettingCommands.KING_VICTIM_ATTACK_SAFE)
-        .setDescription(
-          "ADMIN COMMAND: The vampire king will not die if they bite the same player the werewolves kill"
-        )
-        .addBooleanOption((option) =>
-          option
-            .setName("activate")
-            .setRequired(true)
-            .setDescription("Set setting true or false")
-        )
-    )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName(SettingCommands.BODYGUARD_JOINS_MASONS)
-        .setDescription("ADMIN COMMAND: The Bodyguard can join the masons")
-        .addBooleanOption((option) =>
-          option
-            .setName("activate")
-            .setRequired(true)
-            .setDescription("Set setting true or false")
-        )
-    )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName(SettingCommands.SEER_JOINS_MASONS)
-        .setDescription("ADMIN COMMAND: The Seer can join the masons")
-        .addBooleanOption((option) =>
-          option
-            .setName("activate")
-            .setRequired(true)
-            .setDescription("Set setting true or false")
-        )
-    )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName(SettingCommands.HUNTER_GUARD)
-        .setDescription("ADMIN COMMAND: Hunter will kill one werewolf when attacked by werewolves")
-        .addBooleanOption((option) =>
-          option
-            .setName("activate")
-            .setRequired(true)
-            .setDescription("Set setting true or false")
-        )
-  )
-  .addSubcommand((subcommand) =>
-    subcommand
-      .setName(SettingCommands.ALLOW_LYCAN_GUARD)
-      .setDescription("ADMIN COMMAND: Lycan will be protected from one werewolf attack")
-      .addBooleanOption((option) =>
-        option
-          .setName("activate")
-          .setRequired(true)
-          .setDescription("Set setting true or false")
-      )
-  )
-    .addSubcommand((subcommand) =>
-      subcommand
-        .setName(SettingCommands.ENABLE_POWER_UPS)
-        .setDescription("ADMIN COMMAND: players will also get one power up at the start of the game")
-        .addBooleanOption((option) =>
-          option
-            .setName("activate")
-            .setRequired(true)
-            .setDescription("Set setting true or false")
-        )
-    ),
+  return builder
+}
+
+
+module.exports = {
+  data: buildSlashCommand(),
   async execute(interaction) {
     await interaction.deferReply({ ephemeral: true });
     const command = interaction.options.getSubcommand();
@@ -281,7 +103,7 @@ module.exports = {
       });
     }
 
-    if (command === SettingCommands.VIEW) {
+    if (command === valueSettings.VIEW) {
       let message = "";
       delete settings._id;
       delete settings.server_name;
@@ -313,8 +135,8 @@ module.exports = {
     const data = {};
 
     if (
-      command === SettingCommands.DAY_TIME ||
-      command === SettingCommands.NIGHT_TIME
+      command === valueSettings.DAY_TIME ||
+      command === valueSettings.NIGHT_TIME
     ) {
       const hour = interaction.options.getNumber("hour");
       const minute = interaction.options.getNumber("minute");
@@ -345,12 +167,13 @@ module.exports = {
       return;
     }
 
+    const settingValue = interaction.options.getString("setting");
     const activate = interaction.options.getBoolean("activate");
-    data[command] = activate;
+    data[settingValue] = activate;
     await updateSettings(guildId, data);
 
     await interaction.editReply({
-      content: `${command} has been set to ${activate}`,
+      content: `Setting has been set to ${activate}`,
       ephemeral: true,
     });
   },
---
+++ src/util/botMessages/settings.js
@@ -0,0 +1,172 @@
+const SettingCommands = {
+  EXTRA_CHARACTERS: "extra_characters",
+  ALLOW_CHAOS_DEMON: 'allow_chaos_demon',
+  ALLOW_VAMPIRES: "allow_vampires",
+  KING_BITE_WOLF_SAFE: "king_bite_wolf_safe",
+  KING_VICTIM_ATTACK_SAFE: "king_victim_attack_safe",
+  ALLOW_FIRST_BITE: "allow_first_bite",
+  WOLF_KILLS_WITCH: "wolf_kills_witch",
+  ALLOW_LYCAN_GUARD: "allow_lycan_guard",
+  BODYGUARD_JOINS_MASONS: "bodyguard_joins_masons",
+  SEER_JOINS_MASONS: "seer_joins_masons",
+  HUNTER_GUARD: "hunter_guard",
+  ALLOW_REACTIONS: "allow_reactions",
+  RANDOM_CARDS: "random_cards",
+  CAN_WHISPER: "can_whisper",
+  HARD_MODE: "hard_mode",
+  ENABLE_POWER_UPS: "enable_power_ups",
+};
+
+module.exports = {
+  SettingCommands,
+  settingsIntro: `# Settings
+  Admins can use the command \`/settings edit\` to turn settings off or on.
+  `,
+  settingsList: [
+    {
+      label: 'Night and Day times',
+      emoji: '⌛',
+      description: `# Night and Day times
+Use these commands (\`/settings night_time\`, \`/settings day_time\`) to set the day and night time in the game. Currently the time zone is set in MST. So you might need to adjust the time if you are in a different time zone.
+`
+    },
+    {
+      id: SettingCommands.EXTRA_CHARACTERS,
+      label: 'Add Extra Characters',
+      emoji: '👥',
+      description: `### Extra Characters
+Add more characters into the game. Characters added:
+* Mutated Villager
+* Witch
+* Fool
+* Werewolf Cub
+* DoppelGanger
+* Apprentice_seer
+* Grouchy Granny
+* Monarch
+`,
+    },
+    {
+      id: SettingCommands.ALLOW_CHAOS_DEMON,
+      label: 'Add Chaos Demon',
+      emoji: '😈',
+      description: `### Add Chaos Demon
+Adds the Chaos Demon in the game.
+`,
+    },
+    {
+      id: SettingCommands.ALLOW_VAMPIRES,
+      label: 'Add King Vampire',
+      emoji: '🧛‍♂️',
+      description: `### Add King Vampire
+Adds the King Vampire in the game.
+`,
+    },
+    {
+      id: SettingCommands.KING_BITE_WOLF_SAFE,
+      label: 'King Vampire biting werewolf',
+      emoji: '🧛‍♂️',
+      description: `### King Vampire biting werewolf
+If this setting is on it will protect the King Vampire if they try and bite a werewolf. They will get a message saying that that user is protect or can defend against their attacks.
+This will only apply to the vampire king and the other vampires will die if they try to bite a werewolf.
+`,
+    },
+    {
+      id: SettingCommands.KING_VICTIM_ATTACK_SAFE,
+      label: 'King Vampire biting prey',
+      emoji: '🧛‍♂️',
+      description: `### King Vampire biting prey
+If this setting is on it will protect the King Vampire if they try and bite a player that is also attacked by a werewolf.
+This will only apply to the vampire king and the other vampires will die if they try to bite a player that is also being attacked by the werewolves.
+`,
+    },
+    {
+      id: SettingCommands.ALLOW_FIRST_BITE,
+      label: 'King Vampire first bite',
+      emoji: '🧛‍♂️',
+      description: `### King Vampire first Bite
+If this setting is on the King Vampire first successful bite will convert a player into a vampire. After that it will take two bites to convert a player into a vampire.
+`,
+    },
+    {
+      id: SettingCommands.WOLF_KILLS_WITCH,
+      label: 'Werewolf can kill Witch',
+      emoji: '🐾',
+      description: `### Werewolf can kill witch
+If this setting is on the werewolf will kill the witch. If this setting is off the witch will not die and join the werewolf channel.
+`,
+    },
+    {
+      id: SettingCommands.ALLOW_LYCAN_GUARD,
+      label: 'Lycan guard',
+      emoji: '🛡️',
+      description: `### Lycan guard
+If this setting is on the lycan will be protected one werewolf attack. After that they will die if the werewolf attacks them. The werewolves will be told if they tired to kill the lycan.
+`,
+    },
+    {
+      id: SettingCommands.BODYGUARD_JOINS_MASONS,
+      label: 'Bodyguard joins Masons',
+      emoji: '💂‍♂️',
+      description: `### Bodyguard joins Masons
+If this setting is on the bodyguard can join the masons channel if they guard one of them.
+`,
+    },
+    {
+      id: SettingCommands.SEER_JOINS_MASONS,
+      label: 'Seer joins Masons',
+      emoji: '🔮',
+      description: `### Seer joins Masons
+If this setting is on the seer can join the masons channel if they investigate one of them.
+`,
+    },
+    {
+      id: SettingCommands.HUNTER_GUARD,
+      label: 'Hunter guard',
+      emoji: '🔫',
+      description: `### Hunter guard
+If this setting is on the hunter will auto kill a random werewolf if they are attacked by one in the night. After that they will die without the ability to shoot their gun. If they die any other way they will be able to shoot their gun.
+`,
+    },
+    {
+      id: SettingCommands.ALLOW_REACTIONS,
+      label: 'Discord Reactions',
+      emoji: '😂',
+      description: `### Discord Reactions
+Because discord lets people add to existing reactions when they can view the channel there is a chance to cheat when dead. If this setting is off it will disable reactions so people will not be able to cheat.
+`,
+    },
+    {
+      id: SettingCommands.RANDOM_CARDS,
+      label: 'Random Characters',
+      emoji: '🎲',
+      description: `### Random Characters
+Turn this setting on to make each character random. The game will try its best to balance the teams. The only set characters are the werewolves and if you allow single characters like vampire and chaos demon. Turning this setting off will attempt to use all characters available and if it runs out will start using random characters. 
+`,
+    },
+    {
+      id: SettingCommands.CAN_WHISPER,
+      label: 'Allow Whispers',
+      emoji: '🔉',
+      description: `### Allow Whispers
+Turn this setting on to allow the whisper command to be used. Users will only be able to whisper once per day and it will reset after the day cycle ends.
+`,
+    },
+    {
+      id: SettingCommands.HARD_MODE,
+      label: 'Hard Mode',
+      emoji: '🥲',
+      description: `### Hard Mode
+Turn this setting on to hide the werewolf and vampire count in the who_is_alive command. This will also hide the players character when they are hanged. When a player dies in a different way it will show their character. e.g. when the werewolves kill a player. 
+`,
+    },
+    {
+      id: SettingCommands.ENABLE_POWER_UPS,
+      label: 'Power Ups',
+      emoji: '🦸‍♂️',
+      description: `### Power Ups
+Turn this on to give a power to each player at the start of the game. The powers are the same that the monarch can give. Players are given a one power randomly
+`,
+    },
+  ],
+}
