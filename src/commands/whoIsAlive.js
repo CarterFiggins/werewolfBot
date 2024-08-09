60fa69314a272ce19f0cc9a4ad752101d7eb1440
Carter Figgins <Carter.Figgins@atomicjolt.com>
2024-08-09 17:14:43 -0600

    who is alive upgrade

--- src/commands/whoIsAlive.js
+++ src/commands/whoIsAlive.js
@@ -1,11 +1,12 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const _ = require("lodash");
 const { commandNames } = require("../util/commandHelpers");
-const { characters } = require("../util/characterHelpers/characterUtil")
-const { findUsersWithIds, findSettings } = require("../werewolf_db");
+const { characters } = require("../util/characterHelpers/characterUtil");
+const { findSettings, findAllUsers } = require("../werewolf_db");
 const { getPlayingCount } = require("../util/userHelpers");
 const { permissionCheck } = require("../util/permissionCheck");
 const { getAliveUsersIds } = require("../util/discordHelpers");
+const { channelNames } = require("../util/channelHelpers");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -29,53 +30,76 @@ module.exports = {
 
     const aliveUsersId = await getAliveUsersIds(interaction);
     const members = await interaction.guild.members.fetch();
+    const channel = interaction.guild.channels.cache.get(interaction.channelId);
 
-    const cursor = await findUsersWithIds(interaction.guild.id, aliveUsersId);
+    const cursor = await findAllUsers(interaction.guild.id, aliveUsersId);
     const dbUsers = await cursor.toArray();
-    const settings = await findSettings(interaction.guild.id);
-
-    message = "Players Alive:\n";
-    werewolfCount = 0;
-    villagerCount = 0;
-    vampireCount = 0;
-
-    _.shuffle(dbUsers).forEach((user) => {
-      message += `${members.get(user.user_id)}\n`;
-      if (user.character === characters.WEREWOLF) {
-        werewolfCount += 1;
-      } else if (user.is_vampire) {
-        vampireCount += 1;
-      } else {
-        villagerCount += 1;
-      }
-    });
 
     if (_.isEmpty(dbUsers)) {
-      const {playersCount, playingMembers} = await getPlayingCount(interaction)
+      const { playersCount, playingMembers } = await getPlayingCount(
+        interaction
+      );
       await interaction.reply({
-        content: `Player count: ${playersCount}\n${_.join(playingMembers, "\n")}`,
+        content: `Player count: ${playersCount}\n${_.join(
+          playingMembers,
+          "\n"
+        )}`,
         ephemeral: false,
         allowedMentions: {
-          parse: []
-        }
+          parse: [],
+        },
       });
       return;
     }
 
-    const vampireMessage = vampireCount
-      ? `Vampire Count: ${vampireCount}\n`
-      : "";
-    
+    const settings = await findSettings(interaction.guild.id);
+
+    let message = "## Players Alive:\n";
+    let deadMessage = "## Players Dead:\n";
+    let werewolfCount = 0;
+    let villagerCount = 0;
+    let vampireCount = 0;
+    let someoneIsDead = false;
+
+    _.shuffle(dbUsers).forEach((user) => {
+      let characterMessage = "";
+      if (channel.name === channelNames.AFTER_LIFE) {
+        const vampireMessage = user.is_vampire ? "vampire " : "";
+        characterMessage = `: **${vampireMessage}${user.character}**`;
+      }
+      if (!user.is_dead) {
+        message += `${members.get(user.user_id)}${characterMessage}\n`;
+        if (user.character === characters.WEREWOLF) {
+          werewolfCount += 1;
+        } else if (user.is_vampire) {
+          vampireCount += 1;
+        } else {
+          villagerCount += 1;
+        }
+      } else {
+        someoneIsDead = true;
+        deadMessage += `${members.get(user.user_id)}${characterMessage}\n`;
+      }
+    });
+
     if (!settings.hard_mode) {
-      message += `Werewolf Count: ${werewolfCount}\n${vampireMessage}Villager Count: ${villagerCount}`;
+      const werewolfMessage = werewolfCount
+        ? `Werewolf Count: ${werewolfCount}\n`
+        : "";
+
+      const vampireMessage = vampireCount
+        ? `Vampire Count: ${vampireCount}\n`
+        : "";
+
+      message += `${werewolfMessage}${vampireMessage}Villager Count: ${villagerCount}\n`;
     }
 
     await interaction.reply({
-      content: message,
+      content: `${message}${someoneIsDead ? deadMessage : ""}`,
       ephemeral: false,
       allowedMentions: {
-        parse: []
-      }
+        parse: [],
+      },
     });
   },
 };
